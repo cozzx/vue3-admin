1bb69500a907ee57daefc521a0f2160d25650014
zhangtao <codeplay@163.com>
2024-07-10 21:51:46 +0800

    feat(config): add element-plus and unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ const pathSrc = resolve(__dirname, "src");
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -20,6 +25,12 @@ export default defineConfig({
     AutoImport({
       // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ["vue"],
+      resolvers: [
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        ElementPlusResolver(),
+        // 自动导入图标组件
+        IconsResolver({}),
+      ],
       eslintrc: {
         // 是否自动生成 eslint 规则，建议生成之后设置 false
         enabled: true,
@@ -34,11 +45,24 @@ export default defineConfig({
       // dts: "src/types/auto-imports.d.ts",
     }),
     Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+        // 自动注册图标组件
+        IconsResolver({
+          // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          enabledCollections: ["ep"],
+        }),
+      ],
       // 指定自定义组件位置(默认:src/components)
       dirs: ["src/components", "src/**/components"],
       // 指定自动导入组件TS类型声明文件路径 (false:关闭自动生成)
       dts: false,
       // dts: "src/types/components.d.ts",
+    }),
+    Icons({
+      // 自动安装图标库
+      autoInstall: true,
     }),
   ],
 })
