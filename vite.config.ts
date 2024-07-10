e68e98c2f88035ba10d4d3984f3cc8cba72f184f
zhangtao <codeplay@163.com>
2024-07-10 22:18:32 +0800

    feat(config): add svg icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { UserConfig, ConfigEnv, loadEnv, defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue'
 
 import { resolve } from 'path'
@@ -11,58 +11,67 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': pathSrc
-    },
-  },
-  plugins: [
-    vue(),
-    // 自动导入参考： https://github.com/sxzz/element-plus-best-practices/blob/main/vite.config.ts
-    AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ["vue"],
-      resolvers: [
-        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-        ElementPlusResolver(),
-        // 自动导入图标组件
-        IconsResolver({}),
-      ],
-      eslintrc: {
-        // 是否自动生成 eslint 规则，建议生成之后设置 false
-        enabled: true,
-        // 指定自动导入函数 eslint 规则的文件
-        filepath: "./.eslintrc-auto-import.json",
-        globalsPropValue: true,
+export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
+  return {
+    resolve: {
+      alias: {
+        '@': pathSrc
       },
-      // 是否在 vue 模板中自动导入
-      vueTemplate: true,
-      // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
-      dts: false,
-      // dts: "src/types/auto-imports.d.ts",
-    }),
-    Components({
-      resolvers: [
-        // 自动导入 Element Plus 组件
-        ElementPlusResolver(),
-        // 自动注册图标组件
-        IconsResolver({
-          // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
-          enabledCollections: ["ep"],
-        }),
-      ],
-      // 指定自定义组件位置(默认:src/components)
-      dirs: ["src/components", "src/**/components"],
-      // 指定自动导入组件TS类型声明文件路径 (false:关闭自动生成)
-      dts: false,
-      // dts: "src/types/components.d.ts",
-    }),
-    Icons({
-      // 自动安装图标库
-      autoInstall: true,
-    }),
-  ],
+    },
+    plugins: [
+      vue(),
+      // 自动导入参考： https://github.com/sxzz/element-plus-best-practices/blob/main/vite.config.ts
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        eslintrc: {
+          // 是否自动生成 eslint 规则，建议生成之后设置 false
+          enabled: true,
+          // 指定自动导入函数 eslint 规则的文件
+          filepath: "./.eslintrc-auto-import.json",
+          globalsPropValue: true,
+        },
+        // 是否在 vue 模板中自动导入
+        vueTemplate: true,
+        // 指定自动导入函数TS类型声明文件路径 (false:关闭自动生成)
+        dts: false,
+        // dts: "src/types/auto-imports.d.ts",
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+            enabledCollections: ["ep"],
+          }),
+        ],
+        // 指定自定义组件位置(默认:src/components)
+        dirs: ["src/components", "src/**/components"],
+        // 指定自动导入组件TS类型声明文件路径 (false:关闭自动生成)
+        dts: false,
+        // dts: "src/types/components.d.ts",
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve(pathSrc, "assets/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+    ],
+  }
 })
