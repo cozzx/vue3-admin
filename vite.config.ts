ce6101163a7c80344c481c5fd9a2175fd0f23f8b
zhangtao <codeplay@163.com>
2024-07-11 06:40:37 +0800

    feat(config): add dev env and proxy

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
+export default defineConfig(({mode}: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd());
   return {
     resolve: {
       alias: {
@@ -32,6 +33,24 @@ export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
           additionalData: `
             @use "@/styles/variables.scss" as *;
           `,
+        },
+      },
+    },
+    server: {
+      // 允许IP访问
+      host: "0.0.0.0",
+      // 应用端口 (默认:3000)
+      port: Number(env.VITE_APP_PORT),
+      // 运行是否自动打开浏览器
+      open: true,
+      proxy: {
+        /** 代理前缀为 /dev-api 的请求  */
+        [env.VITE_APP_BASE_API]: {
+          changeOrigin: true,
+          // 接口地址
+          target: env.VITE_APP_API_URL,
+          rewrite: (path) =>
+            path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""),
         },
       },
     },
